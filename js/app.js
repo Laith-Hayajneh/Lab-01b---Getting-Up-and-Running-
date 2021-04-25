a14b347faef21afef10222676914edb3ea603e21
laith-hayajneh <farfesh96@gmail.com>
2021-04-25 15:11:15 +0300

    lab01a

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+let username = prompt('please enter your name ');
+alert (username + '  Is a Nice name ');
+console.log (username);
+let age =prompt('Enter your age ');
+console.log(age);
+alert('I think you are old enough to be here ');
+let fav =prompt(' What is your favorite car ?  ' +username );
+console.log(fav);
+alert (fav +'  It is  a nice car'); 
+let gender = prompt (' What is your gender ?');
+console.log (gender);
+alert ('Greet');
